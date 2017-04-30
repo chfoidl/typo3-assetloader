d83f5bd5ef24712a0748320a0af2625ecc23ccdb
Sethorax <info@sethorax.com>
2017-04-30 20:16:26 +0200

    [TASK] Changed dependencies to support TYPO3 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '7.6.10-8.7.99',
         ]
     ]
 ];
