7e2b2ab92900394eefa2a884052dae746ec714cf
Sethorax <info@sethorax.com>
2017-04-30 20:06:29 +0200

    [TASK] Moved static typoscript setup to ext_typoscript_setup.txt

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('TYPO3_MODE') or die('Access denied.');
-
-/**
- * Add Extension TypoScript
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Assets Loader');
